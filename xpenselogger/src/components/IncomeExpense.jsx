80141582fed3ff92094dcf1c1b73ec80ec64565b
arpitjaswal <iamarpitjaswal@gmail.com>
2023-03-14 18:51:51 +0530

    last step

--- xpenselogger/src/components/IncomeExpense.jsx
+++ xpenselogger/src/components/IncomeExpense.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
 
-const IncomeExpense = () => {
-  return (
-    <>
-        <div className="inc-exp-container">
-            <div id="money-plus" className="money plus">
-                <h4>Income</h4>
-                <p>+$0.00</p>
-                </div>
-            <div id="money-minus" className="money minus">
-                <h4>Expense</h4>
-                <p>-$0.00</p>
-                </div>
-        </div>
-    </>
-  )
+const IncomeExpense=()=>{
+  const {transactions} = useContext(GlobalContext);
+  const amounts = transactions.map(transaction=>transaction.amount);
+  const income = amounts.filter(item=>item>0).reduce((acc,item)=>acc+=item,0).toFixed(2);
+  const expense = amounts.filter(item=>item<0).reduce((acc,item)=>(acc+=item,0)*-1).toFixed(2);
+    return (
+      <>
+          <div className="inc-exp-container">
+              <div id="money-plus" className="money plus">
+                  <h4>Income</h4>
+                  <p>{income}</p>
+                  </div>
+              <div id="money-minus" className="money minus">
+                  <h4>Expense</h4>
+                  <p>{expense}</p>
+                  </div>
+          </div>
+      </>
+    )
 }
 
 export default IncomeExpense;
